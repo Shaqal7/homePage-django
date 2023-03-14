f4257c501c13ad59e5532d8ec77075e12b89181f
Tomasz Śmierzchała <tsmierzchala@gmail.com>
2023-03-14 21:21:53 +0100

    Add registration functionality

--- website/main/urls.py
+++ website/main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('home', views.home, name='home'),
     # path('login/', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
-    # path('register/', views.register, name='register'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {'form': form})
